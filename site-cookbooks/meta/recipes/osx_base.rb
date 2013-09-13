931cf78771a6192da37696a4fd7b1a3ed0d5b9ba
Max Prokopiev <max-prokopiev@yandex.ru>
2013-09-13 13:50:07 +0400

    Customize recipies

--- site-cookbooks/meta/recipes/osx_base.rb
+++ site-cookbooks/meta/recipes/osx_base.rb
@@ -1,4 +1,3 @@
-include_recipe "sprout-osx-base::bash_it"
 include_recipe "pivotal_workstation::create_var_chef_cache"
 include_recipe "pivotal_workstation::sshd_on"
 include_recipe "pivotal_workstation::screen_sharing_app"
